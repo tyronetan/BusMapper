fde190849637089cd9cf60cb2334bec8e41f9e35
Tyrone Tan <new_overlord@yahoo.com>
2018-03-17 14:39:15 +0800

    Set up continuous integration script

--- jakefile.js
+++ jakefile.js
@@ -1,7 +1,21 @@
+/* global desc, task, jake, fail, complete */
+
+"use strict";
+
 task("default", ["lint"]);
 
 desc("lint everything");
 task("lint", [], function() {
 	var lint = require("./build/lint/lint_runner.js");
-	lint.validateFile("jakefile.js", {}, {});
+
+	var files = new jake.FileList();
+	files.include("**/*.js");
+	files.exclude("node_modules");
+	files.exclude("build");
+
+	var options = {
+		node: true
+	};
+
+	lint.validateFileList(files.toArray(), options, {});
 });
