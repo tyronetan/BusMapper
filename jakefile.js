6dafbc1e311a7e3a96801e03fcf47f96a91d62ae
Tyrone Tan <new_overlord@yahoo.com>
2018-03-17 14:39:59 +0800

    Set up continuous integration script

--- jakefile.js
+++ jakefile.js
@@ -19,3 +19,16 @@ task("lint", [], function() {
 
 	lint.validateFileList(files.toArray(), options, {});
 });
+
+
+desc("integrate");
+task("integrate", ["default"], function() {
+	console.log("1. make sure git status is clean");
+	console.log("2. Build on the integration box");
+	console.log("	a. Walk over to integration box");
+	console.log("	b. git pull");
+	console.log("	c. jake");
+	console.log("3. git checkout integration");
+	console.log("4. git merge master --no-ff --log");
+	console.log("5. git checkout master");
+});
