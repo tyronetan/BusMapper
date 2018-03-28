36905f8cb114240aa6367da565c7de4f1a0a4800
Tyrone Tan <new_overlord@yahoo.com>
2018-03-28 18:49:19 +0800

    made unit tests for creating a hello world server

--- src/server/server.js
+++ src/server/server.js
@@ -1,3 +1,20 @@
 "use strict";
+var http = require("http");
 
-console.log("Hi, I am a server.");
+
+var server;
+
+exports.start = function() {
+	server = http.createServer();
+	server.on("request", function(request, response) {
+		console.log("Received request");
+
+		response.end("Hello World");
+	});
+
+	server.listen(8880);
+};
+
+exports.stop = function(callback) {
+	server.close(callback);
+};
--- tests/unit/server/server.js
+++ tests/unit/server/server.js
@@ -1,10 +1,37 @@
 /* jshint node: true */
 
 "use strict";
-var expect = require("chai").expect;
+var chai = require("chai"),
+	should = chai.should(),
+	expect = chai.expect;
+var assert = require("assert");
+var server = require("../../../src/server/server.js");
+var http = require("http");
+var chaiHttp = require("chai-http");
+
+
+chai.use(chaiHttp);
 
 describe("server", function() {
-	it("should pass on 3 = 3", function() {
+	it("should pass on 3 = 3", function(done) {
 		expect(3).to.equals(3);
+		done();
+	});
+
+	it("should get a hello world response on a request to server", (done) => {
+		server.start(8080);
+		let receivedData = false;
+
+		chai.request("http://localhost:8880")
+			.get("/")
+			.end((err, res) => {
+				expect(res).to.have.status(200);
+				res.text.should.be.eql("Hello World");
+				done();
+			});
+	});
+
+	after(function() {
+		server.stop(function() {});
 	});
 });
